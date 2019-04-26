797fd3d8a9be77727faa6a160b8696915794185b
Roland <roland.meertens@gmail.com>
2019-04-27 01:06:10 +0200

    pil image

--- inference.py
+++ inference.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 
 # from datasets import imagenet
 import PIL
+import picamera
+import picamera.array
 
 import numpy as np
 img = np.array(PIL.Image.open('panda.jpg').resize((96,96))).astype(np.float) / 128 - 1
@@ -21,3 +23,22 @@ with tf.Session(graph=inp.graph):
 # label_map = imagenet.create_readable_names_for_imagenet_labels()
 print(x.argmax())
 # print("Top 1 Prediction: ", x.argmax(),label_map[x.argmax()], x.max())
+
+
+
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.resolution = (640, 480)
+
+        while True:
+            camera.capture(stream, 'rgb', use_video_port=True)
+            stream.flush()
+            # stream.array now contains the image data in BGR order
+            new_im = PIL.Image.fromarray(stream.array)
+            img = np.array(new_im.resize((96,96))).astype(np.float) / 128 - 1
+            print(img)
+            # reset the stream before the next capture
+            stream.seek(0)
+            stream.truncate()
+
+        cv2.destroyAllWindows()
