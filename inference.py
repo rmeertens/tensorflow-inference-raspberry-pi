7acb1d91fc4fbe4ec5ba469eb7dcd7e36f2279e7
Roland <roland.meertens@gmail.com>
2019-04-27 01:07:31 +0200

    inference as well

--- inference.py
+++ inference.py
@@ -25,20 +25,21 @@ print(x.argmax())
 # print("Top 1 Prediction: ", x.argmax(),label_map[x.argmax()], x.max())
 
 
+with tf.Session(graph=inp.graph):
+    with picamera.PiCamera() as camera:
+        with picamera.array.PiRGBArray(camera) as stream:
+            camera.resolution = (640, 480)
 
-with picamera.PiCamera() as camera:
-    with picamera.array.PiRGBArray(camera) as stream:
-        camera.resolution = (640, 480)
+            while True:
+                camera.capture(stream, 'rgb', use_video_port=True)
+                stream.flush()
+                # stream.array now contains the image data in BGR order
+                new_im = PIL.Image.fromarray(stream.array)
+                img = np.array(new_im.resize((96,96))).astype(np.float) / 128 - 1
+                x = predictions.eval(feed_dict={inp: img.reshape(1, 96,96, 3)})
+                print(x.argmax())
+                # reset the stream before the next capture
+                stream.seek(0)
+                stream.truncate()
 
-        while True:
-            camera.capture(stream, 'rgb', use_video_port=True)
-            stream.flush()
-            # stream.array now contains the image data in BGR order
-            new_im = PIL.Image.fromarray(stream.array)
-            img = np.array(new_im.resize((96,96))).astype(np.float) / 128 - 1
-            print(img)
-            # reset the stream before the next capture
-            stream.seek(0)
-            stream.truncate()
-
-        cv2.destroyAllWindows()
+            cv2.destroyAllWindows()
